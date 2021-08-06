4fb8fb185304f1e4d396b0f4182f10b28887d13b
Jacobkao <coffeegarden123@gmail.com>
2021-08-05 23:20:57 -0400

    Update httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -2,6 +2,6 @@
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo sudo yum install git
+sudo yum install git
 sudo yum install java-1.8.0-openjdk-devel -y
-sudo yum install gedit
+
